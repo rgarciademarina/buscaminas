b8e0e5bd858e5a86d445c53e1b14bc5e98220788
rgarciademarina <raulgmm@ext.inditex.com>
2025-03-02 16:20:50 +0100

    Configuración para despliegue en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: '/buscaminas/', // Base path for GitHub Pages
 });
