493a3c4d3c057424cb48c2fe726ed230038ab78b
rgarciademarina <raulgmm@ext.inditex.com>
2025-03-02 16:33:33 +0100

    Crea versión estática del juego para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import { reactRouter } from 'vite-plugin-react-router';
+import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   base: '/buscaminas/', // Base path for GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        main: './app/static.tsx', // Punto de entrada para la versión estática
+      },
+    },
+    outDir: 'dist', // Directorio de salida para la versión estática
+  },
 });
